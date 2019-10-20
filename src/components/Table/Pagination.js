665a438ae85ddb41e563262494d8ba5eb5f9bc83
Ahmet Kuslular <ahmet.kuslular@iyzico.com>
2019-10-20 23:09:39 +0300

    fixed pagination

--- src/components/Table/Pagination.js
+++ src/components/Table/Pagination.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export class Pagination extends React.Component {
+export class Pagination extends Component {
   static defaultProps = {
     page: 1,
     loading: false,
@@ -23,23 +23,13 @@ export class Pagination extends React.Component {
     const currentPage = parseInt(page, 10);
     const hasPrev = page > 1;
     const hasNext = currentPage < pageCount;
-    console.log({ currentPage, pageCount });
+
     return (
       <StyledPagination>
-        <PaginationButton
-          beveled
-          disabled={!hasPrev}
-          uppercase={false}
-          onClick={() => this.handleClick(currentPage - 1)}
-        >
+        <PaginationButton disabled={!hasPrev} onClick={() => this.handleClick(currentPage - 1)}>
           Prev
         </PaginationButton>
-        <PaginationButton
-          beveled
-          disabled={!hasNext}
-          uppercase={false}
-          onClick={() => this.handleClick(currentPage + 1)}
-        >
+        <PaginationButton disabled={!hasNext} onClick={() => this.handleClick(currentPage + 1)}>
           Next
         </PaginationButton>
       </StyledPagination>
@@ -57,5 +47,36 @@ const StyledPagination = styled.div`
 `;
 
 const PaginationButton = styled.button`
+  font-size: 12px;
+  color: #395564;
+  padding: 0.58em 0;
+  border-radius: 0.5em;
+  border-color: #395564;
+  font-weight: normal;
+  padding: 0.5em;
+  outline: none;
+  cursor: pointer;
   margin-left: 1.25em;
+  &:hover {
+    transform: translateY(-1px);
+    box-shadow: 0px 2px 3px -1px #ddd;
+    color: #f50;
+    border-color: #f50;
+  }
+  ${props =>
+    props.disabled &&
+    `
+      color: #395564;
+      background-color: white;
+      opacity: 0.5;
+      cursor: not-allowed;
+      
+      &:hover {
+        transform: none;
+        box-shadow: none;
+        border-color: #395564;
+        color:#395564;
+        opacity:0.5;
+      }
+  `}
 `;
