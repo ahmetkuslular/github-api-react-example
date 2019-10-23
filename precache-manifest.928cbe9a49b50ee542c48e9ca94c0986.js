52bb8f83225ba602f318064ae07d24c743d8a82c
Ahmet <ahmet@Cagatay-MacBook-Air.local>
2019-10-24 01:37:27 +0300

    Updates

---
+++ precache-manifest.928cbe9a49b50ee542c48e9ca94c0986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd076f47b345a4ed613f4a5d43557b8",
+    "url": "/github-api-react-example/index.html"
+  },
+  {
+    "revision": "24ad528175c51a77bb57",
+    "url": "/github-api-react-example/static/css/main.1a3f95e5.chunk.css"
+  },
+  {
+    "revision": "050a94595cfb56be9a9f",
+    "url": "/github-api-react-example/static/js/2.4f8a4fe3.chunk.js"
+  },
+  {
+    "revision": "24ad528175c51a77bb57",
+    "url": "/github-api-react-example/static/js/main.f631f605.chunk.js"
+  },
+  {
+    "revision": "c2924dc0d86d386af70a",
+    "url": "/github-api-react-example/static/js/runtime-main.6c24c792.js"
+  }
+]);
